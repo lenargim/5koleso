ad101889644f96d56faee981aedf2c3f8bc9b02f
Lenar <linz14@mail.ru>
2022-03-05 16:10:17 +0300

    add open list button

--- app/entry.php
+++ app/entry.php
@@ -360,6 +360,22 @@ include('partials/header.php'); ?>
         controls: []
       }),
 
+      ListOpenLayout = ymaps.templateLayoutFactory.createClass(["<div class='list-layout'></div>"].join());
+    buttonListOpen = new ymaps.control.Button({
+      data: {
+        content: "Жмак-жмак-жмак",
+        image: 'images/pen.png',
+        title: "Жмак-жмак-жмак"
+      },
+      options: {
+        layout: ListOpenLayout,
+      }
+    });
+
+    myMap.controls.add(buttonListOpen);
+    buttonListOpen.events.add('click',function () {
+      $('.entry__info').removeClass('hide');
+    })
       ZoomLayout = ymaps.templateLayoutFactory.createClass("<div class='map-layout'>" +
         "<div id='zoom-in' class='btn'><svg class='icon-plus' width=\"16\" height=\"16\" viewBox=\"0 0 16 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
         "<path d=\"M15 7H9V1C9 0.734784 8.89464 0.48043 8.70711 0.292893C8.51957 0.105357 8.26522 0 8 0C7.73478 0 7.48043 0.105357 7.29289 0.292893C7.10536 0.48043 7 0.734784 7 1V7H1C0.734784 7 0.48043 7.10536 0.292893 7.29289C0.105357 7.48043 0 7.73478 0 8C0 8.26522 0.105357 8.51957 0.292893 8.70711C0.48043 8.89464 0.734784 9 1 9H7V15C7 15.2652 7.10536 15.5196 7.29289 15.7071C7.48043 15.8946 7.73478 16 8 16C8.26522 16 8.51957 15.8946 8.70711 15.7071C8.89464 15.5196 9 15.2652 9 15V9H15C15.2652 9 15.5196 8.89464 15.7071 8.70711C15.8946 8.51957 16 8.26522 16 8C16 7.73478 15.8946 7.48043 15.7071 7.29289C15.5196 7.10536 15.2652 7 15 7Z\" fill=\"white\"/>\n" +
@@ -424,7 +440,7 @@ include('partials/header.php'); ?>
             }
           );
         }
-      }),
+      });
       zoomControl = new ymaps.control.ZoomControl({options: {layout: ZoomLayout}});
     myMap.controls.add(zoomControl, {
       position: {}
