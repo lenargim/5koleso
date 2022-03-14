d280f146ca6125f9f475a40aa136f585eace4c41
Lenar <linz14@mail.ru>
2022-03-14 04:02:30 +0300

    News detailed page

---
+++ app/news-detailed.php
@@ -0,0 +1,50 @@
+<?php include('partials/head.php');
+include('partials/header-registered.php'); ?>
+  <main class="main main-bg news-detailed">
+    <div class="container">
+      <div class="breadcrumbs">
+        <a href="/">Главная</a>
+        <a href="news.php">Новости</a>
+        <a href="news-detailed.php">Работаем круглосуточно</a>
+      </div>
+      <div class="news-detailed__wrap">
+        <h1>Работаем круглосуточно</h1>
+        <div class="news-detailed__narrow">
+          <img src="img/news-detailed1.jpg" alt="Личный транспорт – спасение во время изоляции.">
+          <h2>Личный транспорт – спасение во время изоляции.</h2>
+          <div class="news-detailed__text">
+            <p>Друзья, в ближайшую неделю нам всем крайне важно ограничить контакты с другими людьми. Если все же
+              пришлось выйти, лучше воспользоваться личным транспортом.
+              Далеко не все успели подготовить свои колеса к теплому сезону. Поэтому мы будем работать в усиленном
+              режиме, чтобы обеспечить безопасность вас и ваших машин на дороге!
+              Рук и ресурсов хватит на все машины в городе!
+              Все сотрудники компании
+              “5 колесо” носят защитные маски и регулярно обрабатывают руки и инструменты антисептиками.
+              Ждем вас!</p>
+          </div>
+        </div>
+        <div class="news-detailed__wide">
+          <div class="img">
+            <img src="img/news-detailed2.jpg" alt="Работаем круглосуточно">
+          </div>
+          <div class="news-detailed__item">
+            <div class="news-detailed__img img"><img src="img/news-detailed3.jpg" alt="Работаем круглосуточно"></div>
+            <div class="news-detailed__desc">При движении автомобиля балансировка колес напрямую влияет на работу
+              подвески и трансмиссии. Дисбаланс колес приводит не только к дополнительным тратам на техническое
+              обслуживание ТС, но и на комфорт при езде. Биение колеса — это ударная нагрузка на ступицу и подшипники,
+              на все трущиеся и катящиеся детали.
+            </div>
+          </div>
+          <div class="news-detailed__item">
+            <div class="news-detailed__img img"><img src="img/news-detailed4.jpg" alt="Работаем круглосуточно"></div>
+            <div class="news-detailed__desc">Правильная балансировка колес — одна из основ безопасности, длительности
+              срока службы ходовой части вашего автомобиля и комфорта при езде.
+              Бесплатная проверка на балансировку (со съемом/установкой)
+            </div>
+          </div>
+        </div>
+        <?php include('partials/footer-nav.php'); ?>
+      </div>
+    </div>
+  </main>
+<?php include('partials/footer.php'); ?>
